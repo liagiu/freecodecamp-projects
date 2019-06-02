98764a569788b7866b37a9797edf5af2ea6d8233
Giulia <giu.olivers@gmail.com>
2019-06-01 21:40:16 -0300

    Add responsiveness

--- responsive-web-design-projects/survey-form/gulpfile.js
+++ responsive-web-design-projects/survey-form/gulpfile.js
@@ -1,45 +1,54 @@
-const gulp          = require ('gulp'),
-      sass          = require ('gulp-sass'),
-      babel         = require('gulp-babel'),
-      uglify        = require('gulp-uglify'),
-      sourcemaps    = require('gulp-sourcemaps'),
-      autoprefixer  = require('gulp-autoprefixer'),
-      browserSync   = require('browser-sync').create();
-
-const reload = browserSync.reload;
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync').create(),
+  reload = browserSync.reload;
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', () => {
-  gulp.src('./src/scss/main.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./dist'));
-});
+function css() {
+  return gulp
+    .src('./src/scss/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+}
 
-gulp.task('js', () => {
-  gulp.src('./src/main.js')
-  .pipe(sourcemaps.init())
-  .pipe(babel())
-  .pipe(uglify())
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./dist'));
-});
+function js() {
+  return gulp
+    .src('./src/main.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist'));
+}
 
-gulp.task('watch', ['sass'], () => {
+function watchFiles() {
   browserSync.init({
     server: {
       baseDir: './'
     }
   });
-  gulp.watch('src/scss/*.scss', ['sass']).on('change', reload);
-  gulp.watch('*.html').on('change', reload);
-  gulp.watch('src/main.js', ['js']).on('change', reload);
-});
+  gulp.watch('src/scss/*.scss', css);
+  gulp.watch('src/main.js', js);
+  gulp
+    .watch(['*.html', 'dist/main.css', 'dist/main.js'])
+    .on('change', browserSync.reload);
+}
 
-gulp.task('default', ['sass', 'js', 'watch']);
+exports.watchFiles = watchFiles;
+exports.css = css;
+exports.js = js;
+exports.default = watchFiles;
--- responsive-web-design-projects/survey-form/src/main.js
+++ responsive-web-design-projects/survey-form/src/main.js
@@ -1,4 +1,3 @@
-
 const form = document.querySelector('.form');
 const formBtns = document.querySelector('.form__btns');
 const arrowBtns = document.querySelectorAll('.form__arrow-btn');
@@ -6,55 +5,68 @@ const dotBtns = document.querySelectorAll('.form__dot-btn');
 const submitBtn = document.querySelector('.form__submit-btn');
 const tabs = document.querySelectorAll('.form__tab');
 const alert = document.querySelector('.form__alert');
+const header = document.querySelector('.header');
+const wrapper = document.querySelector('.wrapper');
 let currTab = 0;
 
 function validateForm() {
-  const inputs = [...tabs[currTab].querySelectorAll('.form__input, .form__dropdown')];
+  const inputs = [
+    ...tabs[currTab].querySelectorAll('.form__input, .form__dropdown')
+  ];
   const radioBtns = tabs[currTab].querySelectorAll('.form__radio');
-  const checkboxes = tabs[currTab].querySelectorAll('.form__checkbox');  
+  const checkboxes = tabs[currTab].querySelectorAll('.form__checkbox');
   let isRadioChecked = false;
   let isCheckboxChecked = false;
   let isInputFilled = true;
 
   for (let input of inputs) {
-    console.log(input);
-    if(!input.value) {
+    if (!input.value) {
       input.classList.add('form__invalid');
       isInputFilled = false;
       break;
-    };
+    }
     input.classList.remove('form__invalid');
   }
-  
+
   radioBtns.forEach(radioBtn => {
-    if(radioBtn.checked) return isRadioChecked = true;
+    if (radioBtn.checked) return (isRadioChecked = true);
   });
   checkboxes.forEach(checkbox => {
-    if(checkbox.checked) return isCheckboxChecked = true;
+    if (checkbox.checked) return (isCheckboxChecked = true);
   });
 
-  if(!isInputFilled || (radioBtns.length > 0 && !isRadioChecked) || (checkboxes.length > 0 && !isCheckboxChecked)) { 
+  if (
+    !isInputFilled ||
+    (radioBtns.length > 0 && !isRadioChecked) ||
+    (checkboxes.length > 0 && !isCheckboxChecked)
+  ) {
     alert.style.opacity = 1;
     return false;
-  } else { 
+  } else {
     alert.style.opacity = 0;
-    return true; 
+    return true;
   }
 }
 
 function switchTab(btn) {
   const btnIndex = btn.target.value;
-  
-  if(btnIndex === '1') { if(!validateForm()) return; }
+
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  alert.style.opacity = 0;
+
+  if (btnIndex === '1') {
+    if (!validateForm()) return;
+  }
 
   tabs.forEach(tab => {
     tab.style.opacity = 0;
     tab.style.zIndex = 0;
   });
 
-  if(btnIndex === '0' && currTab > 0) {
+  if (btnIndex === '0' && currTab > 0) {
     currTab -= 1;
-  } else if (btnIndex === '1' && currTab < tabs.length-2) {
+  } else if (btnIndex === '1' && currTab < tabs.length - 2) {
     currTab += 1;
   }
 
@@ -62,25 +74,26 @@ function switchTab(btn) {
   tabs[currTab].style.zIndex = 1;
   colorDotBtn(currTab);
 
-  if(btnIndex === '0' && currTab === 0) { 
-    btn.target.style.color = 'rgba(62, 78, 86, 0.6)'; 
+  if (btnIndex === '0' && currTab === 0) {
+    btn.target.style.color = 'rgba(62, 78, 86, 0.6)';
   } else {
-    arrowBtns[0].style.color = '#3e4e56';
+    arrowBtns[0].style.color = '#607682';
   }
 
-  if(btnIndex === '1' && currTab === 2) { 
-    btn.target.style.color = 'rgba(62, 78, 86, 0.6)'; 
+  if (btnIndex === '1' && currTab === 2) {
+    btn.target.style.color = 'rgba(62, 78, 86, 0.6)';
   } else {
-    arrowBtns[1].style.color = '#3e4e56';
+    arrowBtns[1].style.color = '#607682';
   }
-  
-  formBtns.style.top = `${tabs[currTab].offsetHeight + 59}px`
+
+  formBtns.style.top = `${tabs[currTab].offsetHeight +
+    formBtns.offsetHeight -
+    25}px`;
 }
 
-
 function colorDotBtn(btnIndex) {
-  for(let i = 0; i < tabs.length -1; i++) {
-    if(i <= btnIndex) {
+  for (let i = 0; i < tabs.length - 1; i++) {
+    if (i <= btnIndex) {
       dotBtns[i].style.background = '#3e4e56';
     } else {
       dotBtns[i].style.background = '#fff';
@@ -90,7 +103,7 @@ function colorDotBtn(btnIndex) {
 
 function handleSubmit(e) {
   e.preventDefault();
-  if(validateForm() && currTab == 2) {
+  if (validateForm() && currTab == 2) {
     tabs[currTab].style.opacity = 0;
     tabs[3].style.opacity = 1;
     tabs[3].style.zIndex = 4;
@@ -102,4 +115,13 @@ arrowBtns.forEach(btn => btn.addEventListener('click', switchTab));
 form.addEventListener('submit', handleSubmit);
 submitBtn.addEventListener('click', handleSubmit);
 
-window.addEventListener('orientationchange', () => setTimeout(() => formBtns.style.top = `${tabs[currTab].offsetHeight + 63}px`, 200));
+window.addEventListener('resize', () => {
+  console.log(wrapper.offsetWidth);
+  formBtns.style.opacity = 0;
+  setTimeout(() => {
+    formBtns.style.top = `${tabs[currTab].offsetHeight +
+      formBtns.offsetHeight -
+      25}px`;
+    formBtns.style.opacity = 1;
+  }, 600);
+});
